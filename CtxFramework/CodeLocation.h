bab9633d87ad9d8b2941d3d546e84fd2e239d59d
Leonardo Marques <Leonardo.Marques@t-systems-mms.com>
2015-12-13 22:56:54 +0100

    added missing codelocation class

---
+++ CtxFramework/CodeLocation.h
@@ -0,0 +1,17 @@
+//
+//  CodeLocation.h
+//  
+//
+//  Created by Leonardo Marques on 03.10.15.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import "Behavior.h"
+
+@interface CodeLocation : NSObject{
+    NSNumber* locationType;
+    Behavior* behavior;
+}
+
+@end
---
+++ CtxFramework/CodeLocation.m
@@ -0,0 +1,19 @@
+//
+//  CodeLocation.m
+//  
+//
+//  Created by Leonardo Marques on 03.10.15.
+//
+//
+
+#import "CodeLocation.h"
+
+@interface CodeLocation (){
+    NSDictionary* jsonCodeLocation;  
+}
+
+@end
+
+@implementation CodeLocation
+
+@end
